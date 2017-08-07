b218aea42553a676ffaeafcdbe0438e2da81b01d
Vlad Zadvorny <vladzadvorny@icloud.com>
2017-08-07 22:01:47 +0300

    fix bug

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const querystring = require('querystring');
 const httpBuildQuery = require('http-build-query');
 
-module.exports = (method, params) => {
-  const get = arguments[2] !== undefined ? arguments[2] : false;
+module.exports = (method, params, get) => {
+  get = get || false;
   params.v = params.v || '5.67';
 
   return get

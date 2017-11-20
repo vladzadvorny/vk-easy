f705177d6f74d3491aef76f5370e1a4106949eff
Vlad Zadvorny <vladzadvorny@icloud.com>
2017-11-20 15:40:21 +0300

    request is replaced with axios

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const rp = require('request-promise');
+const axios = require('axios');
+const querystring = require('querystring');
+const httpBuildQuery = require('http-build-query');
 
 module.exports = (method, params, get) => {
   get = get || false;
   params.v = params.v || '5.67';
 
   return get
-    ? rp({
-        uri: `https://api.vk.com/method/${method}`,
-        qs: params,
-        json: true
-      })
-    : rp({
-        method: 'POST',
-        uri: `https://api.vk.com/method/${method}`,
-        formData: params,
-        json: true
-      });
+    ? axios.get(`https://api.vk.com/method/${method}?${httpBuildQuery(params)}`)
+    : axios.post(
+        `https://api.vk.com/method/${method}`,
+        querystring.stringify(params)
+      );
 };
